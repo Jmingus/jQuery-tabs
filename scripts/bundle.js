ee27e3c64e555d2225ba1d95c73d4a41b4fc6486
Jmingus <jmingus@austin.rr.com>
2015-09-14 23:07:27 -0500

    Finished Nightmare mode

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -8,7 +8,18 @@ $links.click(function () {
   var currentLink = $(this).context.firstChild.data.toLowerCase();
   $('#' + currentLink + '-block').show().siblings().hide();
 });
+
 window.location.hash = hash;
+window.onload = function () {
+  $(hash + '-block').show().siblings().hide();
+  if ($links.prevObject[0].URL === "http://localhost:8000/#blue") {
+    $($links[1]).parent().addClass('active').siblings().removeClass('active');
+  } else if ($links.prevObject[0].URL === "http://localhost:8000/#red") {
+    $($links[0]).parent().addClass('active').siblings().removeClass('active');
+  } else if ($links.prevObject[0].URL === "http://localhost:8000/#green") {
+    $($links[2]).parent().addClass('active').siblings().removeClass('active');
+  }
+};
 
 },{}]},{},[1])
 
