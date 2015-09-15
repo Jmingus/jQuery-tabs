1b275992bfb02a6c8ac760507c01fe3eb9479e0f
Jmingus <jmingus@austin.rr.com>
2015-09-14 21:53:54 -0500

    Almost done with nightmare mode

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,5 +1,14 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
+var $links = $('a');
+var hash = window.location.hash;
+
+$links.click(function () {
+  $(this).parent('li').addClass('active').siblings().removeClass('active');
+  var currentLink = $(this).context.firstChild.data.toLowerCase();
+  $('#' + currentLink + '-block').show().siblings().hide();
+});
+window.location.hash = hash;
 
 },{}]},{},[1])
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+var $links = $('a')
+var hash = window.location.hash;
+
+$links.click(function()  {
+  $(this).parent('li').addClass('active').siblings().removeClass('active');
+  var currentLink = $(this).context.firstChild.data.toLowerCase();
+  $('#' + currentLink + '-block').show().siblings().hide();
+});
+window.location.hash = hash
+
